97216c1354d9944515a93c91dfc571aa81585a39
Mantra Gohil <mantragohil@mantra.local>
2023-06-18 15:26:56 +0530

    mantras changes

--- index.js
+++ index.js
@@ -13,19 +13,53 @@ app.get('/', (req, res) => {
 // thing that is left is to emit to all the connected users that the particular user is connected.
 
 
+var users = [];
 
 // only works if some event listeners on client side are not there.
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        io.emit('chat message', (msg))
-    })
+        socket.broadcast.emit('chat message', (msg))
+    });
 })
 
 io.on('connection', (socket) => {
+  users.push({id: socket.id, name: "user" + socket.id});
+  console.log('a user connected', users);
+  
+  socket.on("disconnect", (SOCKET) => {
+    console.log(users)
+    users.map((value)=>{
+      if(value.id == socket.id){
+        console.log("user disconnected", value.name);
+      }
+  });
+  });
+
   socket.on('connect', (msg) => {
     io.emit('chat message', (msg))
   })
+
+  
+});
+
+io.on('connection', (socket) => {
+  socket.on("someone-connected", (username)=>{
+    console.log("someone connected, ready to send users list", username);
+    socket.broadcast.emit("someone-connected-msg", username);
+  });
+
+  socket.on("set-username", (username)=>{
+    console.log("username received from", socket.id, username)
+    users.map((value)=>{
+      if(value.id == socket.id){
+        value.name = username;
+      }
+    });
+  });
+
 })
+
+
 
 // io.on('connection', (socket) => {
 //   socket.on('disconnect', (msg) => {
